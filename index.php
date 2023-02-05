1873fb5379bab779e0238ff61c072026431a329c
VictorSed <vicsedglez818@gmail.com>
2023-02-04 22:00:28 -0600

    JAS

--- index.php
+++ index.php
@@ -4,14 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicio</title>
 </head>
 <body>
 
+<style>
+
+    p{
+        margin: 45px;
+        padding: 20px;
+        border: none;
+        background-color:teal;
+        color: white;
+        border-radius: 20px;
+        font-size: 1.4em;
+    }
+
+</style>
+
     <h2>Hola a todos</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus labore
-         quo, quasi soluta, consequatur nisi blanditiis ipsa incidunt fugit excepturi 
-         doloremque vel qui consectetur illum et, suscipit accusantium saepe maiores.
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus labore
+        quo, quasi soluta, consequatur nisi blanditiis ipsa incidunt fugit excepturi 
+        doloremque vel qui consectetur illum et, suscipit accusantium saepe maiores.
+    </p>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus labore
+        quo, quasi soluta, consequatur nisi blanditiis ipsa incidunt fugit excepturi 
+        doloremque vel qui consectetur illum et, suscipit accusantium saepe maiores.
+    </p>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus labore
+        quo, quasi soluta, consequatur nisi blanditiis ipsa incidunt fugit excepturi 
+        doloremque vel qui consectetur illum et, suscipit accusantium saepe maiores.
     </p>
     
 </body>
