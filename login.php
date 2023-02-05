cc5a9e0087f222ef711484b2933314bb8f889a8a
VictorSed <vicsedglez818@gmail.com>
2023-02-04 21:26:09 -0600

    Login

--- login.php
+++ login.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Ingresar</title>
 </head>
 <body>
+
+    <h1>Iniciar  sesión</h1>
+
+    <form action="" style="text-align: center; border:2px solid black; margin:50px; padding: 15px;">
+        <label for="">Correo</label><br>
+        <input type="email">
+
+        <br><hr style="width: 50%;">
+
+        <label for="">Contraseña</label><br>
+        <input type="password" name="" id="">
+    </form>
     
 </body>
 </html>
